08b259ff599a1b86dcb40c51c8762e65df432925
nhanhas <mpereira132@hotmail.com>
2017-11-05 16:32:10 +0000

    S#2 Data no footer

--- QuizzController.js
+++ QuizzController.js
@@ -1,14 +1,47 @@
 /* On Start App */
 jQuery(document).ready(function($){
+	updateQuizzDateTime();
 	resetApp();//reset defaults
 });
 
 //function to reset application
 function resetApp(){
 	$('#home').css('display','block');
-	$('#intro').css('display','none');
+	$('#app').css('display','none');
 }
 
+//function to update quizz date and hour
+function updateQuizzDateTime(){
+	var date = new Date();
+
+	var monthNames = [
+	"Jan", "Fev", "Mar",
+	"Abr", "Maio", "Jun", "Jul",
+	"Ago", "Set", "Out",
+	"Nov", "Dez"  ];
+
+	//day, month year
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	//day of week
+	var options = { weekday: 'long'};
+	var weekDay = date.toLocaleDateString('pt-PT', options)
+
+
+	var dateString = weekDay + '. ' + day + ' ' + monthNames[monthIndex] + ' ' + year;
+
+	var time = new Date();
+	var timeOptions = { hour: '2-digit', minute: '2-digit' };
+	var timeString = time.toLocaleTimeString('pt-PT', timeOptions);
+
+
+	//now update our display footer
+	jQuery('#quizz-date').html(dateString);
+	jQuery('#quizz-hour').html(timeString);
+
+}
 
 
 /* Home Controlling... */
@@ -17,6 +50,6 @@ function homeTouch(){
 
 	//switch views
 	$('#home').css('display','none');
-	$('#intro').css('display','block');
+	$('#app').css('display','block');
 
 }
