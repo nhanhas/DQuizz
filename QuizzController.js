8c65218e3fb6b68bae85e814c2ee3e0bb08311a6
nhanhas <mpereira132@hotmail.com>
2017-11-12 14:34:16 +0000

    #S15 almost done

--- QuizzController.js
+++ QuizzController.js
@@ -1,3 +1,5 @@
+var TIMMER_TO_REFRESH = 5000; //5sec
+
 /* On Start App */
 jQuery(document).ready(function($){
 	updateQuizzDateTime();
@@ -5,6 +7,7 @@ jQuery(document).ready(function($){
 	resetApp();//reset defaults
 	
 });
+
 
 
 function startKeyboard() {
@@ -97,6 +100,17 @@ function homeTouch(){
 	//switch views
 	$('#home').css('display','none');
 	$('#app').css('display','block');
+
+}
+
+function goToCredits(){
+	/**/$('#view-results').css('display','none');
+	/**/$('#view-end').css('display','block');
+
+	//Start Timer to re-start
+	setTimeout(function(){ 
+		location.reload(); 
+	}, TIMMER_TO_REFRESH);
 
 }
 
@@ -201,6 +215,15 @@ function generateQuestionTemplate(question, isLastQuestion){
 
 //#4.2 - (AUX) Generate questions template
 function generateOptionsTemplate(question, options){
+
+	//aux func to return custom style
+	var getStyleCustom = function (optionText){
+		if(optionText.length <= 39){
+			return 'style="padding-top: 30px;"'
+		}
+		return '';
+	}
+
 	var template = '\
 			<!-- Questions -->\
 			<!-- 1 2 -->\
@@ -208,13 +231,13 @@ function generateOptionsTemplate(question, options){
 				<div class="col-xs-6">\
 					<p>\
 						<input type="radio" id="'+options[0].optId+'" name="'+options[0].optId+'">\
-						<label class="font-bold font-36" for="'+options[0].optId+'">'+options[0].description+'</label>\
+						<label '+getStyleCustom(options[0].description)+' class="font-bold font-36" for="'+options[0].optId+'">'+options[0].description+'</label>\
 					</p>\
 				</div>\
 				<div class="col-xs-6">\
 					<p>\
 						<input type="radio" id="'+options[1].optId+'" name="'+options[1].optId+'">\
-						<label class="font-bold font-36" for="'+options[1].optId+'">'+options[1].description+'</label>\
+						<label '+getStyleCustom(options[1].description)+' class="font-bold font-36" for="'+options[1].optId+'">'+options[1].description+'</label>\
 					</p>\
 				</div>\
 			</div>\
@@ -224,13 +247,13 @@ function generateOptionsTemplate(question, options){
 				<div class="col-xs-6 text-white">\
 				<p>\
 					<input type="radio" id="'+options[2].optId+'" name="'+options[2].optId+'">\
-					<label class="font-bold font-36" for="'+options[2].optId+'">'+options[2].description+'</label>\
+					<label '+getStyleCustom(options[2].description)+' class="font-bold font-36" for="'+options[2].optId+'">'+options[2].description+'</label>\
 				</p>\
 				</div>\
 				<div class="col-xs-6">\
 				<p>\
 					<input type="radio" id="'+options[3].optId+'" name="'+options[3].optId+'">\
-					<label class="font-bold font-36" for="'+options[3].optId+'">'+options[3].description+'</label>\
+					<label '+getStyleCustom(options[3].description)+' class="font-bold font-36" for="'+options[3].optId+'">'+options[3].description+'</label>\
 				</p>\
 				</div>\
 			</div>';
@@ -333,36 +356,84 @@ function setPercentage(circleReference, percentage) {
 function DB_getQuestions(){
 	var questionsList = [
 		{ id : 0,
-		  ask : "Esta é a pergunta número 1", 
-		  solution : "Q0-2", 
-		  options : [	{optId : "Q0-1", description: "opção 1 da pergunta 1 opção 1 da pergunta 1"},
-		  				{optId : "Q0-2", description: "opção 2 da pergunta 1"},
-		  				{optId : "Q0-3", description: "opção 3 da pergunta 1"},
-		  				{optId : "Q0-4", description: "opção 4 da pergunta 1"}		] 
+		  ask : "A maioria dos portugueses espera que a economia nacional, no próximo ano, evolua de forma...", 
+		  solution : "Q0-1", 
+		  options : [	{optId : "Q0-1", description: "Positiva"},
+		  				{optId : "Q0-2", description: "Negativa"},
+		  				{optId : "Q0-3", description: "Neutra"},
+		  				{optId : "Q0-4", description: "Não sabem"}		] 
 		},
 		{ id : 1,
-		  ask : "Esta é a pergunta número 2", 
-		  solution : "Q1-4", 
-		  options : [	{optId : "Q1-1", description: "opção 1 da pergunta 2"},
-		  				{optId : "Q1-2", description: "opção 2 da pergunta 2"},
-		  				{optId : "Q1-3", description: "opção 3 da pergunta 2"},
-		  				{optId : "Q1-4", description: "opção 4 da pergunta 2"}		] 
+		  ask : "Grande parte dos consumidores nacionais considera que o seu poder de compra, face a 2016...", 
+		  solution : "Q1-3", 
+		  options : [	{optId : "Q1-1", description: "Aumentou"},
+		  				{optId : "Q1-2", description: "Diminui"},
+		  				{optId : "Q1-3", description: "Manteve-se"},
+		  				{optId : "Q1-4", description: "Não sabem"}		] 
 		},
 		{ id : 2,
-		  ask : "Esta é a pergunta número 3", 
-		  solution : "Q2-3", 
-		  options : [	{optId : "Q2-1", description: "opção 1 da pergunta 3"},
-		  				{optId : "Q2-2", description: "opção 2 da pergunta 3"},
-		  				{optId : "Q2-3", description: "opção 3 da pergunta 3"},
-		  				{optId : "Q2-4", description: "opção 4 da pergunta 3"}		] 
+		  ask : "Em que medida o Orçamento do Estado influencia as expetativas e comportamentos da maioria dos portugueses?", 
+		  solution : "Q2-2", 
+		  options : [	{optId : "Q2-1", description: "Muito"},
+		  				{optId : "Q2-2", description: "Moderado"},
+		  				{optId : "Q2-3", description: "Pouco"},
+		  				{optId : "Q2-4", description: "Não sabem"}		] 
 		},
 		{ id : 3,
-		  ask : "Esta é a pergunta número 4", 
+		  ask : "Quanto espera a maioria dos portugueses gastar nesta época festiva do Natal e Ano Novo?", 
 		  solution : "Q3-1", 
-		  options : [	{optId : "Q3-1", description: "opção 1 da pergunta 4"},
-		  				{optId : "Q3-2", description: "opção 2 da pergunta 4"},
-		  				{optId : "Q3-3", description: "opção 3 da pergunta 4"},
-		  				{optId : "Q3-4", description: "opção 4 da pergunta 4"}		] 
+		  options : [	{optId : "Q3-1", description: "Entre 300€ - 350€"},
+		  				{optId : "Q3-2", description: "Entre 350€ - 400€"},
+		  				{optId : "Q3-3", description: "Entre 400€ - 450€"},
+		  				{optId : "Q3-4", description: "Entre 450€ - 500€"}		] 
+		},
+		{ id : 4,
+		  ask : "Grande parte dos portugueses espera gastar a maior parte do seu orçamento de Natal em...", 
+		  solution : "Q4-3", 
+		  options : [	{optId : "Q4-1", description: "Alimentação e bebidas"},
+		  				{optId : "Q4-2", description: "Eventos sociais"},
+		  				{optId : "Q4-3", description: "Presentes"},
+		  				{optId : "Q4-4", description: "Viagens"}		] 
+		},
+		{ id : 5,
+		  ask : "Qual o presente que a maioria dos portugueses considera mais provável receber neste Natal?", 
+		  solution : "Q5-1", 
+		  options : [	{optId : "Q5-1", description: "Chocolates"},
+		  				{optId : "Q5-2", description: "Cosméticos/ Perfumes"},
+		  				{optId : "Q5-3", description: "Dinheiro"},
+		  				{optId : "Q5-4", description: "Roupa/ Calçado"}		] 
+		},
+		{ id : 6,
+		  ask : "Em que locais a maioria dos portugueses prefere fazer as suas compras de Natal?", 
+		  solution : "Q6-2", 
+		  options : [	{optId : "Q6-1", description: "Cadeias de retalho especializado"},
+		  				{optId : "Q6-2", description: "Centros comerciais"},
+		  				{optId : "Q6-3", description: "Hipermercados/ supermercados"},
+		  				{optId : "Q6-4", description: "Lojas de rua"}		] 
+		},
+		{ id : 7,
+		  ask : "Que fonte a maioria dos portugueses preferem utilizar para procurar ideias e conselhos para as suas compras de Natal?", 
+		  solution : "Q7-3", 
+		  options : [	{optId : "Q7-1", description: "Canais informais (amigos, familiares, vizinhos)"},
+		  				{optId : "Q7-2", description: "Internet"},
+		  				{optId : "Q7-3", description: "Lojas físicas"},
+		  				{optId : "Q7-4", description: "Redes sociais"}		] 
+		},
+		{ id : 8,
+		  ask : "Grande parte dos portugueses considera que a principal utilidade das redes sociais, para as compras de Natal, é...", 
+		  solution : "Q8-2", 
+		  options : [	{optId : "Q8-1", description: "Encontrar descontos, cupões e informações de vendas"},
+		  				{optId : "Q8-2", description: "Pesquisar produtos"},
+		  				{optId : "Q8-3", description: "Verificar preços"},
+		  				{optId : "Q8-4", description: "Verificar quais os presentes que a família e amigos pretendem"}		] 
+		},
+		{ id : 9,
+		  ask : "Para melhorar a sua experiência de compra, a maioria dos portugueses gostaria que os retalhistas investissem em...", 
+		  solution : "Q9-4", 
+		  options : [	{optId : "Q9-1", description: "Aplicação de telemóvel"},
+		  				{optId : "Q9-2", description: "Entrega em casa"},
+		  				{optId : "Q9-3", description: "Horários mais alargados"},
+		  				{optId : "Q9-4", description: "Preços mais baixos"}		] 
 		}
 
 	];
